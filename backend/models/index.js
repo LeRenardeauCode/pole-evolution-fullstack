0abc8bb8055d789e846bfbe249005d86b9274d8a
= <=>
2026-01-22 15:40:39 +0100

    ADD: ajout d'un index.js afin d'importer et exporter tout les models

---
+++ backend/models/index.js
@@ -0,0 +1,21 @@
+import Utilisateur from './Utilisateur.js';
+import Cours from './Cours.js';
+import Reservation from './Reservation.js';
+import Forfait from './Forfait.js';
+import Media from './Media.js';
+import Avis from './Avis.js';
+import MessageContact from './MessageContact.js';
+import Notification from './Notification.js';
+import Parametre from './Parametre.js';
+
+export {
+  Utilisateur,
+  Cours,
+  Reservation,
+  Forfait,
+  Media,
+  Avis,
+  MessageContact,
+  Notification,
+  Parametre
+};
